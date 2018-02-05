cc629915b684af379ac6a1ed0002de28e2087050
lobbeque <darkglobit@gmail.com>
2018-02-05 17:35:16 +0100

    tyty

--- stayingAlive/scrape-yabiladi.py
+++ stayingAlive/scrape-yabiladi.py
@@ -3,6 +3,7 @@ import requests
 import json
 import os
 import re
+import urllib2
 from urllib2 import urlopen
 import bs4 as BeautifulSoup
 import unicodedata
@@ -14,14 +15,24 @@ import unicodedata
 # 	print "\nError !! try : python archive-to-url-tree.py yabiladi.com 100\n"
 # 	sys.exit(0)
 
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+
 nb_list = 100
 
 def getPages(url):
-	# print url
-	try :
-		html = urlopen(url,data=None,timeout=10).read()
-		return html
-	except :
+	print url
+	req = urllib2.Request(url, headers=hdr)
+
+	try:
+		page = urllib2.urlopen(req)
+		return html.read()
+	except urllib2.HTTPError, e:
+		print e.fp.read()
 		return None
 
 while nb_list > 0 :
